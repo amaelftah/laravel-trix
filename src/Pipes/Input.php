2295ae96aad8b7c427647c84e814c33926eaf074
Teodor B <11290723+teodoriu@users.noreply.github.com>
2020-11-12 12:48:06 +0200

    Update Input.php (#48)
    Restore old value on failed validation

--- src/Pipes/Input.php
+++ src/Pipes/Input.php
@@ -8,7 +8,7 @@ class Input
 {
     public function handle(LaravelTrix $trix, \Closure $next)
     {
-        $html = is_object($trix->model) && $trix->model->exists ? htmlspecialchars(optional($trix->model->trixRichText()->where('field', $trix->config['field'])->first())->content, ENT_QUOTES) : '';
+        $html = is_object($trix->model) && $trix->model->exists ? htmlspecialchars(optional($trix->model->trixRichText()->where('field', $trix->config['field'])->first())->content, ENT_QUOTES) : old($trix->loweredModelName.'-trixFields.'.$trix->config['field'], '');
 
         $trix->html .= "<input id='{$trix->config['id']}' value='{$html}' name='{$trix->loweredModelName}-trixFields[{$trix->config['field']}]' type='hidden'>";
 
