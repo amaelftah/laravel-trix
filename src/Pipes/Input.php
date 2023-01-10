86dec91a777f649b80f1f3acfdf8c6d7a05fa538
PITCHAYAKIT-LEG\Pitchayakit <pitchayakit.p@gmail.com>
2023-01-10 10:37:46 +0700

    Fixd old value doesn't load when using attachments

--- src/Pipes/Input.php
+++ src/Pipes/Input.php
@@ -8,7 +8,10 @@ class Input
 {
     public function handle(LaravelTrix $trix, \Closure $next)
     {
-        $html = is_object($trix->model) && $trix->model->exists ? htmlspecialchars(optional($trix->model->trixRichText()->where('field', $trix->config['field'])->first())->content, ENT_QUOTES) : old($trix->loweredModelName.'-trixFields.'.$trix->config['field'], '');
+        $html = is_object($trix->model) && $trix->model->exists ? optional($trix->model->trixRichText()->where('field', $trix->config['field'])->first())->content : old($trix->loweredModelName.'-trixFields.'.$trix->config['field'], '');
+        
+        //Convert both double and single quotes.
+        $html = htmlspecialchars($html, ENT_QUOTES);
 
         $trix->html .= "<input id='{$trix->config['id']}' value='{$html}' name='{$trix->loweredModelName}-trixFields[{$trix->config['field']}]' type='hidden'>";
 
